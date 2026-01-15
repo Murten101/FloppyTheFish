482129abb723108615e8f9d5fd4904856bd0d2d4
Jerpharney <jerpharney@gmail.com>
2026-01-15 12:50:16 +0100

    cinematic

--- Assets/Script/ActionTrigger.cs
+++ Assets/Script/ActionTrigger.cs
@@ -4,6 +4,29 @@ public class ActionTrigger : MonoBehaviour
 {
     [SerializeField]
     private GameObject TargetUi;
+    public bool StartAnim = false;
+
+    [SerializeField]
+    private Animator anim;
+
+    void Start()
+    {
+        anim.speed = 0;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            StartAnim = true;
+
+        if(StartAnim)
+        {
+            Debug.Log("animation started");
+            anim.speed = 1;
+            StartAnim = false;
+        }
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
