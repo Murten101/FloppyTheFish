926a6592c0c9dfcece10d6f9e1d20eaf6f6e1d0d
Maarten <65980689+Murten101@users.noreply.github.com>
2026-01-30 15:50:15 +0100

    Implement score display

---
+++ Assets/Script/GetFastestTime.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+public class GetFastestTime : MonoBehaviour
+{
+    [SerializeField] TMP_Text _targetText;
+
+    private void Start()
+    {
+        var time = PlayerPrefs.GetFloat(GameTimer.FastestRunTimeKey, -1);
+        var timeString = time == -1 ? "-" : time.ToString();
+        _targetText.text = $"Best: {timeString}";
+    }
+}
---
+++ Assets/Script/GetLastTime.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+public class GetLastTime : MonoBehaviour
+{
+    [SerializeField] TMP_Text _targetText;
+
+    private void Start()
+    {
+        var time = PlayerPrefs.GetFloat(GameTimer.LastRunTimeKey, -1);
+        var timeString = time == -1 ? "-" : time.ToString();
+        _targetText.text = $"Last: {timeString}";
+    }
+}
